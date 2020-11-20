a0764befeb318c32ca4e37ca59f8578d078a4a3e
Karan Jariwala <karankjariwala@gmail.com>
2020-11-20 13:54:26 -0800

    More sm changes

--- PyTorch/Segmentation/MaskRCNN/pytorch/tools/train_net_sm.py
+++ PyTorch/Segmentation/MaskRCNN/pytorch/tools/train_net_sm.py
@@ -7,14 +7,6 @@ import shutil
 import json
 
 
-# def parse_args():
-#     parser = argparse.ArgumentParser(description='Get model info')
-#     parser.add_argument('--num_nodes', type=int, help='Number of nodes')
-#     parser.add_argument('--model', type=str, help='One of mrcnn or bert')
-#     parser.add_argument('--data_mode', type=str, help='One of fsx or s3')
-#     args = parser.parse_args()
-#     return args
-
 def main():
     parser = argparse.ArgumentParser(description="PyTorch Object Detection Training")
     parser.add_argument(
@@ -25,6 +17,7 @@ def main():
         type=str,
     )
     parser.add_argument("--local_rank", type=int, default=os.getenv('LOCAL_RANK', 0))
+    parser.add_argument("--num-nodes", dest="num_nodes", type=int, help="Number of worker nodes")
     parser.add_argument("--max_steps", type=int, default=0,
                         help="Override number of training steps in the config")
     parser.add_argument("--skip-test", dest="skip_test", help="Do not test the final model",
@@ -59,21 +52,16 @@ def main():
     args, unknown = parser.parse_known_args()
     main_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'train_net.py'))
     config_file = os.path.join('/opt/ml/code', args.config_file)
-    num_nodes = len(os.environ["SM_HOSTS"])
+    num_nodes = args.num_nodes
     num_gpus = int(os.environ["SM_NUM_GPUS"])
     hosts = json.loads(os.environ["SM_HOSTS"])
     current_host = os.environ["SM_CURRENT_HOST"]
     rank = hosts.index(current_host)
     work_dir = os.environ['SM_OUTPUT_DATA_DIR']
+    os.environ['PYTHONPATH'] = os.path.abspath(
+        os.path.dirname(os.path.dirname(__file__))) + os.pathsep + os.environ['PYTHONPATH']
 
     data_dir = "/opt/ml/input/data/train/"
-    json_file = work_dir + "/dllogger.json"
-
-    # cmd = f"python -m torch.distributed.launch --nnodes={num_nodes}  --node_rank={rank}  --nproc_per_node={num_gpus} \
-    #     --master_addr={hosts[0]} --master_port='1234' \
-    #     /opt/ml/code/DeepLearningExamples/PyTorch/Segmentation/MaskRCNN/pytorch/tools/train_net.py --data-dir {data_dir} \
-    #     --config-file /opt/ml/code/test_config.yaml --seed 987 --skip-test DTYPE float16 OUTPUT_DIR {work_dir}"
-
 
     cmd = (
         f'python'
@@ -92,12 +80,7 @@ def main():
         f' OUTPUT_DIR {work_dir}'
     )
 
-    start_timestamp = datetime.datetime.now()
-    start_timestamp = start_timestamp.strftime("%Y-%m-%d-%H-%M-%S")
-
-    start = time.time()
     invoke_train(cmd)
-    end = time.time()
 
 def invoke_train(cmd):
     process = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
@@ -112,26 +95,5 @@ def invoke_train(cmd):
         raise subprocess.CalledProcessError(returncode=process.returncode, cmd=cmd)
 
 
-def export_to_s3(work_dir, model_dir, totaltime, model_name, timestamp):
-    print("################################")
-    print(f"Time taken is: {totaltime}")
-    print("################################")
-
-    time_file = os.path.join(work_dir, "timetaken")
-
-    with open(time_file, 'w') as file:
-        file.write(str(totaltime))
-
-    dst = os.path.join(model_dir, "timetaken")
-    shutil.copyfile(time_file, dst)
-
-
 if __name__ == "__main__":
-
-    # args = parse_args()
-    # num_nodes = args.num_nodes
-    # model = args.model
-    # data_mode = args.data_mode
-
     main()
-    # export_to_s3(work_dir, os.environ['SM_MODEL_DIR'], end-start, args.model_name, start_timestamp)
